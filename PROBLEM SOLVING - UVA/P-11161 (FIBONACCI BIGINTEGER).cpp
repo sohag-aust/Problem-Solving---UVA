6a27e9ac6c9fa5d4f16f07d0f964247ee028e2d2
shohag <frustratedcoder96@gmail.com>
2019-07-08 01:11:34 +0600

    Updated

---
+++ PROBLEM SOLVING - UVA/P-11161 (FIBONACCI BIGINTEGER).cpp
@@ -0,0 +1,91 @@
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        BigInteger a = BigInteger.valueOf(1);
+        BigInteger b = BigInteger.valueOf(1);
+        BigInteger c,min_range,max_range,first,last,sum;
+        BigInteger temp = BigInteger.valueOf(1);
+
+        Map<BigInteger, String> mp = new HashMap<>();
+        StringBuffer sb = new StringBuffer("");
+
+        BigInteger cnt = BigInteger.valueOf(2);
+
+        while(true)
+        {
+            c = a.add(b);
+            a = b;
+            b = c;
+            cnt = cnt.add(BigInteger.valueOf(1));
+
+            min_range = temp.add(BigInteger.ONE);
+            max_range = temp.add(c);
+            temp = temp.add(c);
+
+            String s1 = min_range.toString();
+            String s2 = max_range.toString();
+            String str = s1 + ":";
+            str = str + s2;
+
+            mp.put(cnt, str);
+
+            if(cnt.compareTo(BigInteger.valueOf(1500)) == 1)
+                break;
+        }
+
+        BigInteger n;
+        Scanner sc = new Scanner(System.in);
+
+        int i=1;
+        while(sc.hasNext())
+        {
+            n = sc.nextBigInteger();
+
+            if(n.compareTo(BigInteger.ZERO) == 0)
+                break;
+
+            sb.append("Set ").append(i).append(":\n");
+
+            if(n.compareTo(BigInteger.valueOf(1)) == 0)
+                sb.append(0).append("\n");
+            else if(n.compareTo(BigInteger.valueOf(2)) == 0)
+                sb.append(1).append("\n");
+
+            else
+            {
+                String str = mp.get(n);
+                int ind = str.indexOf(":");
+
+                String st1 = str.substring(0,ind);
+                String st2 = str.substring(ind+1,str.length());
+
+                max_range = new BigInteger(st2);
+                min_range = new BigInteger(st1);
+
+                last = max_range.multiply((max_range.add(BigInteger.ONE)));
+                last = last.divide(BigInteger.valueOf(2));
+
+                first = min_range.multiply((min_range.subtract(BigInteger.ONE)));
+                first = first.divide(BigInteger.valueOf(2));
+
+                sum = last.subtract(first);
+                c = max_range.subtract(min_range);
+                c = c.add(BigInteger.ONE);
+                sum = sum.divide(c);
+
+                sb.append(sum).append("\n");
+            }
+
+            i++;
+        }
+
+        System.out.print(sb);
+    }
+}
+
